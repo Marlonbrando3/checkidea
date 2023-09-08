b99b80cd5cddea5b823729423bec845b1e22fdba
Marek <marek@MacBook-Air-Marek.local>
2023-09-08 10:48:35 +0200

    first commit

--- src/Main/claim.tsx
+++ src/Main/claim.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Claim() {
   return (
-    <div className='md:w-1/2 w-auto h-full border flex justify-center mt-[180px]'>
+    <div className='md:w-1/2 w-auto h-full border flex justify-center md:items-center md:mt-[180px]'>
         <div className='flex flex-col'>
             <span className='text-[58px] leading-[40px] bg-red-600 text-white mb-[3px]'>We can</span>
             <span className='text-[50px] leading-[45px] bg-red-600 text-white mb-[3px]'>help you</span>
