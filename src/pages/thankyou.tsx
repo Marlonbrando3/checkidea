0e1ab80df0de96387681923ad259c39d8eff1429
Marek <marek@MacBook-Air-Marek.local>
2023-09-08 11:12:56 +0200

    first commit

---
+++ src/pages/thankyou.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Thankyou () {
+  return (
+    <div className='w-screen h-screen'>
+        <div className='h-[90%] w-[70%] flex flex-col items-center justify-center mx-auto'>
+            <p className='text-[30px] leading-9 mb-[40px]'>Dziękujemy za wyrażenie chęci do uzupełenia ankiety, za chwilę nasz system mailingowy przesle link do ankiety. </p>
+            <p>Pozdrawiamy, zespół <b>Check Idea.</b></p>
+        </div>
+    </div>
+  )
+}
